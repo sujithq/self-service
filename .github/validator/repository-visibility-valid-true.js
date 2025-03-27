a6abe69808781bc4aff1256c1ca5730cf97e5c76
Nick Alteen <ncalteen@github.com>
2025-03-27 13:58:21 -0400

    Fix check

--- .github/validator/repository-visibility-valid-true.js
+++ .github/validator/repository-visibility-valid-true.js
@@ -5,8 +5,15 @@
  * @returns {Promise<string>} An error message or `'success'`
  */
 export default async (field) => {
-  if (['internal', 'private', 'public'].includes(field.towLowerCase()))
-    return 'success'
+  if (typeof field === 'string')
+    if (['internal', 'private', 'public'].includes(field.towLowerCase()))
+      return 'success'
+    else 'Must be one of the following: internal, private, public'
+  else if (Array.isArray(field))
+    if (field.length === 0) return 'Missing required field'
+    else if (field.length > 1) return 'Must be a single value'
+    else if (['internal', 'private', 'public'].includes(field[0].toLowerCase()))
+      return 'success'
 
-  return `Invalid Repository Visibility (Must be 'internal', 'private', or 'public'): **${field}**`
+  return 'Must be one of the following: internal, private, public'
 }
